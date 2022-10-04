344e0de11a23b00dc8417df94cda23951e2d4f16
joeychuavertex <joey.chua@vertexholdings.com>
2022-10-04 21:41:18 +0800

    Add loops example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
 
+const Welcome = () => {
+    const greeting = 'Welcome to React';
+    return <div>{greeting}</div>;
+};
+
+const Data = () => {
+    const animals = [
+        { id: 1, name: "Dog"},
+        { id: 2, name: "Cat"},
+    ]
+    return (
+        <ul>
+            {animals && // Only render if there's data - see 'Conditional Rendering'
+                animals.map(({ id, name }) => {
+                    return <li key={id}>{name}</li>;
+                })}
+        </ul>
+    )
+}
+
 const App = () => {
     const name = "John Doe"
+
     return (
         <>
             <div>Hello World!</div>
             <div>{ name }</div>
             <div>{ 2 + 2 }</div>
+            <Welcome/>
+            <Data/>
         </>
     )
 };
