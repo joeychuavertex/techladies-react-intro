eccc43fc244b3e103bd9ae5450bef4d97ddeb897
joeychuavertex <joey.chua@vertexholdings.com>
2022-10-04 21:18:52 +0800

    Add static and dynamic examples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 const App = () => {
-
+    const name = "John Doe"
     return (
         <>
+            <div>Hello World!</div>
+            <div>{ name }</div>
+            <div>{ 2 + 2 }</div>
         </>
     )
 };
