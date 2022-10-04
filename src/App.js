e43b2e441c085f35a6aedc141c92fa43c84d4555
joeychuavertex <joey.chua@vertexholdings.com>
2022-10-05 01:00:01 +0800

    Add lifecycle example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const Welcome = () => {
     const greeting = 'Welcome to React';
@@ -20,6 +20,51 @@ const Data = () => {
     )
 }
 
+const Countdown = ({ hr, min, sec }) => {
+    const [over, setOver] = useState(false);
+    const [paused, setPaused] = useState(true);
+    const [[h, m, s], setTime] = useState([hr, min, sec]);
+
+    const tick = () => {
+        if (paused || over) {
+            return;
+        }
+        if (h === 0 && m === 0 && s === 0) {
+            setOver(true);
+        } else if (m === 0 && s === 0) {
+            setTime([h - 1, 59, 59]);
+        } else if (s === 0) {
+            setTime([h, m - 1, 59]);
+        } else {
+            setTime([h, m, s - 1]);
+        }
+    };
+
+    const handleReset = () => {
+        setTime([hr, min, sec]);
+        setPaused(true);
+        setOver(false);
+    };
+
+    const handlePause = () => setPaused(!paused);
+
+    const fmt = (val) => val.toString().padStart(2, '0');
+
+    useEffect(() => {
+        let ticker = setInterval(() => tick(), 1000);
+        return () => {
+            clearInterval(ticker);
+        };
+    });
+
+    return (
+        <>
+            <h3 className="countdown">{`${fmt(h)}:${fmt(m)}:${fmt(s)}`}</h3>
+            <button onClick={handlePause}>{paused ? 'Start' : 'Pause'}</button>
+            <button onClick={handleReset}>Reset</button>
+        </>
+    );
+};
 
 const App = () => {
     const name = "John Doe"
@@ -34,6 +79,7 @@ const App = () => {
         setCount(count + 1);
     };
 
+
     return (
         <>
             <div>Hello World!</div>
@@ -41,9 +87,13 @@ const App = () => {
             <div>{ 2 + 2 }</div>
             <Welcome/>
             <Data/>
-            <h3>Current count: {count}</h3>
-            <h3>Previous count: {prevCount}</h3>
+            <div>Current count: {count}</div>
+            <div>Previous count: {prevCount}</div>
             <button onClick={handleClick}>Increment</button>
+
+            <div>
+                <Countdown hr={1} min={45} sec={0} />
+            </div>
         </>
     )
 };
